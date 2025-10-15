e4f35b755d8054865f10bc7fd8959fe29130cf88
Tofan Alexandrina <tofanalexandrina22@stud.ase.ro>
2025-10-15 21:22:59 +0300

    completed useEffect in SpecificBoard to use BoardId mock data

---
+++ frontend/src/data/mockBoardImage.js
@@ -0,0 +1,12 @@
+export const mockBoardImage=[
+    {
+        boardImageId:"bi1",
+        boardId: "b1",
+        imageId: "p1"
+    },
+    {
+        boardImageId:"bi2",
+        boardId:"b2",
+        imageId:"p2"
+    }
+]
--- frontend/src/pages/SpecificBoard.jsx
+++ frontend/src/pages/SpecificBoard.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { mockPhotos } from "../data/mockPhotos.js";
 import { mockBoards } from "../data/mockBoards.js";
+import { mockBoardImage } from "../data/mockBoardImage.js";
 import PhotoCard from "../components/PhotoCard.jsx";
 
 const SpecificBoard = () => {
@@ -11,10 +12,26 @@ const SpecificBoard = () => {
 
   //this effect runs whenever the value of boardId changes
   useEffect(() => {
+    //find current board
     const foundBoard = mockBoards.find((board) => board.boardId === boardId);
     setBoard(foundBoard);
-    //here i'll fetch photos for the board from the API
-    setPhotos(mockPhotos);
+
+    //find boardImage relationships for board
+    if (foundBoard) {
+      const boardImageRelationships = mockBoardImage.filter(
+        (bi) => bi.boardId === boardId
+      );
+
+      //get imageIds that belong to this board
+      const boardImageIds = boardImageRelationships.map((bi) => bi.imageId);
+
+      //filter photos, incluse those that belong to board
+      const boardPhotos = mockPhotos.filter((photo) =>
+        boardImageIds.includes(photo.photoId)
+      );
+      //here i'll fetch photos for the board from the API
+      setPhotos(boardPhotos);
+    }
   }, [boardId]);
 
   if (!board) return <div>Loading...</div>;
@@ -33,15 +50,19 @@ const SpecificBoard = () => {
       >
         <h2>{board.boardName}</h2>
       </div>
-      <div style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(4, 1fr)",
-        gap: "10px",
-        margin: "0 auto"
-      }}>
-        {photos.map((photo) => (
-          <PhotoCard key={photo.photoId} photo={photo} />
-        ))}
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(4, 1fr)",
+          gap: "10px",
+          margin: "0 auto",
+        }}
+      >
+        {photos.length > 0 ? (
+          photos.map((photo) => <PhotoCard key={photo.photoId} photo={photo} />)
+        ) : (
+          <div>It's pretty empty here...</div>
+        )}
       </div>
     </div>
   );
