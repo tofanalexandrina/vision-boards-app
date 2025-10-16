f4d2f3853fd759f28a2fd797bd1369c125a49d50
Tofan Alexandrina <tofanalexandrina22@stud.ase.ro>
2025-10-16 15:49:25 +0300

    added hover effect on pictures

--- frontend/src/components/PhotoCard.jsx
+++ frontend/src/components/PhotoCard.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
-const PhotoCard = ({photo}) => {
+const PhotoCard = ({ photo }) => {
+  const [isHovering, setIsHovering] = useState(false);
+
   return (
     <div
       key={photo.id}
       style={{
+        position: "relative",
         backgroundColor: "#eee",
         border: "1px solid black",
         aspectRatio: "1/1",
@@ -14,6 +17,8 @@ const PhotoCard = ({photo}) => {
         alignItems: "center",
         overflow: "hidden",
       }}
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
     >
       <img
         src={photo.imageUrl}
@@ -24,6 +29,31 @@ const PhotoCard = ({photo}) => {
           objectFit: "cover",
         }}
       />
+
+      {isHovering && (
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(255, 255, 255, 0.7)", //70% white
+            padding: "15px",
+            boxSizing: "border-box",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+          }}
+        >
+          <h3 style={{ margin: "0 0 5px 0", color: "#000" }}>{photo.photoName}</h3>
+          <p style={{ margin: 0, fontSize: "14px", color: "#333" }}>
+            {photo.photoDescription || "No description"}
+          </p>
+        </div>
+      )}
     </div>
   );
 };
