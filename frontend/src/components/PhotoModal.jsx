f1f05eb8c418ac269d7d77da61e3a27d0c011163
Tofan Alexandrina <tofanalexandrina22@stud.ase.ro>
2025-10-17 11:22:43 +0300

    created structure for PhotoModal

---
+++ frontend/src/components/PhotoModal.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Modal from "./Modal";
+
+const PhotoModal=({isOpen, onClose, photo})=>{
+if (!photo) return null;
+return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+        <div>
+            <div>
+                <h2>Board</h2>
+            </div>
+            <div>
+                <div>
+                    <img />
+                </div>
+                <div>
+                    <div>{photo.photoName}</div>
+                    <div>{photo.photoDescription || ""}</div>
+                </div>
+            </div>
+        </div>
+    </Modal>
+)
+}
+
+export default PhotoModal;
