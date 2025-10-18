b7bdba283cc49eb75cd7626f14c30b84606f0ab8
Tofan Alexandrina <tofanalexandrina22@stud.ase.ro>
2025-10-18 13:50:57 +0300

    open picture functionality

--- frontend/src/components/PhotoModal.jsx
+++ frontend/src/components/PhotoModal.jsx
@@ -5,16 +5,56 @@ const PhotoModal = ({ isOpen, onClose, photo }) => {
   if (!photo) return null;
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <div>
-        <div>
-          <h2>Board</h2>
+      <div
+        style={{
+          backgroundColor: "white",
+          width: "600px",
+          maxWidth: "100%",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <div style={{ padding: "10px 20px" }}>
+          <h3 style={{ margin: "5px 0", textAlign: "left", opacity: "50%" }}>
+            Board
+          </h3>
         </div>
-        <div>
-          <div>
-            <img />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            padding: "0 20px 20px",
+          }}
+        >
+          <div
+            style={{
+              width: "100%",
+              height: "350px",
+              border: "1px solid black",
+              marginBottom: "10px",
+              overflow: "hidden",
+            }}
+          >
+            <img
+              src={photo.imageUrl}
+              alt={photo.photoName}
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+              }}
+            />
           </div>
-          <div>
-            <div>{photo.photoName}</div>
+          <div
+            style={{
+              border: "1px solid black",
+              width: "100%",
+              padding: "10px",
+              textAlign: "center",
+            }}
+          >
+            <div style={{ fontWeight: "bold" }}>{photo.photoName}</div>
             <div>{photo.photoDescription || ""}</div>
           </div>
         </div>
