cb2e7ed7ece60b083bb3cf36889e42d572be84d2
Tofan Alexandrina <tofanalexandrina22@stud.ase.ro>
2025-10-25 14:03:31 +0300

    created base api layer file

--- frontend/src/services/baseApi.js
+++ frontend/src/services/baseApi.js
@@ -1,2 +1,31 @@
 import axios from 'axios';
 
+//reusable axios instance
+const apiClient=axios.create({
+    //automatically starts request with backend address
+    baseURL: 'http://localhost:5000/api',
+    //sends data as json
+    headers: {
+        'Content-Type':'application/json'
+    },
+    //includes cookies for login sessions
+    withCredentials: true //for backend to recognise the logged-in user - session based auth
+});
+
+//add token before each request if user is logged in
+apiClient.interceptors.request.use((config)=>{
+    const token=localStorage.getItem('token');
+    if (token) config.headers.Authorization=`Bearer ${token}`;
+    return config;
+})
+
+//handle all responses and errors
+apiClient.interceptors.response.use(
+    (response)=>response.data,
+    (error)=>{
+        const message=error.response?.data?.message||'Something went wrong';
+        return Promise.reject(new Error(message));
+    }
+);
+
+export default apiClient;
